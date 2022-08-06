65e0e6b79323cd2600f161ae2720a9743396b1c0
S Alya Mevia <13519083@std.stei.itb.ac.id>
2022-08-06 15:34:16 +0700

    setup

--- build-svg.js
+++ build-svg.js
@@ -60,7 +60,7 @@ const psTime = formatDistance(new Date(2020, 12, 14), today, {
 })
 
 // Today's weather
-const locationKey = '18363_PC'
+const locationKey = '208977'
 let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`
 
 got(url, { prefixUrl: WEATHER_DOMAIN })
